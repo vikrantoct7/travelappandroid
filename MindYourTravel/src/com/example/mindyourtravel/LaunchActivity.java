3feea9fc0c96f638db7fb4ea72142322bd912f90
vikrant23 <vikrant23@gmail.com>
2014-02-09 13:26:19 +0000

    project workspace checked in

---
+++ MindYourTravel/src/com/example/mindyourtravel/LaunchActivity.java
@@ -0,0 +1,39 @@
+package com.example.mindyourtravel;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LaunchActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_launch);
+		final Button btnSingUp =(Button)findViewById(R.id.btnSingUp);
+		btnSingUp.setOnClickListener(onClickBtnSingUp);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.launch, menu);
+		return true;
+	}
+	
+	private OnClickListener onClickBtnSingUp = new OnClickListener()
+	{
+		@Override
+		public void onClick(View view)
+		{
+			Intent intent = new Intent(view.getContext(),RegisterActivity.class);
+			
+			startActivity(intent);
+		}
+	};
+
+}
