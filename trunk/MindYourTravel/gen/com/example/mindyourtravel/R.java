ea5867bfb54e9fef03ee53a3cbaeb27149b8e483
vikrant23@gmail.com <vikrant23@gmail.com@607d1d21-f9f3-96d9-ef0f-9892642b3816>
2014-02-15 18:51:06 +0000

    updated

--- trunk/MindYourTravel/gen/com/example/mindyourtravel/R.java
+++ trunk/MindYourTravel/gen/com/example/mindyourtravel/R.java
@@ -25,36 +25,42 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f080000;
-        public static final int action_settings=0x7f080016;
-        public static final int btnRegister=0x7f080015;
+        public static final int action_settings=0x7f08001a;
+        public static final int btnLoginBack=0x7f080019;
+        public static final int btnLoginSubmit=0x7f080005;
+        public static final int btnRegister=0x7f080016;
         public static final int btnSingUp=0x7f080006;
-        public static final int btnSubmit=0x7f080005;
-        public static final int lblAge=0x7f080012;
+        public static final int btnSubmit=0x7f080007;
+        public static final int lblAge=0x7f080013;
         public static final int lblContactNo=0x7f08000f;
-        public static final int lblFName=0x7f080007;
-        public static final int lblGender=0x7f08000e;
-        public static final int lblLName=0x7f08000a;
+        public static final int lblErrorMsg=0x7f080015;
+        public static final int lblFName=0x7f08000c;
+        public static final int lblGender=0x7f080017;
+        public static final int lblLName=0x7f080009;
         public static final int lblLogin=0x7f080002;
         public static final int lblPassword=0x7f080004;
-        public static final int lblUserName=0x7f080009;
+        public static final int lblRegMsg=0x7f080018;
+        public static final int lblUserName=0x7f08000b;
         public static final int rdoFemale=0x7f080011;
         public static final int rdoGender=0x7f08000d;
         public static final int rdoMale=0x7f080010;
-        public static final int txtAge=0x7f080013;
-        public static final int txtFName=0x7f080008;
-        public static final int txtLName=0x7f08000b;
+        public static final int txtAge=0x7f080012;
+        public static final int txtFName=0x7f08000e;
+        public static final int txtLName=0x7f080008;
         public static final int txtLogin=0x7f080001;
         public static final int txtPassword=0x7f080003;
         public static final int txtPhNo=0x7f080014;
-        public static final int txtUserName=0x7f08000c;
+        public static final int txtUserName=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_launch=0x7f030000;
         public static final int activity_register=0x7f030001;
+        public static final int activity_registration_confimation=0x7f030002;
     }
     public static final class menu {
         public static final int launch=0x7f070000;
         public static final int register=0x7f070001;
+        public static final int registration_confimation=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -63,15 +69,22 @@ public final class R {
         public static final int btnSubmit=0x7f050004;
         public static final int genderFemale=0x7f05000b;
         public static final int genderMale=0x7f05000a;
+        public static final int hello_world=0x7f050015;
         public static final int lblAge=0x7f05000c;
         public static final int lblContactNo=0x7f05000d;
+        public static final int lblError=0x7f05000f;
+        public static final int lblErrorTechnical=0x7f050011;
+        public static final int lblErrorUserExist=0x7f050010;
         public static final int lblFName=0x7f050007;
         public static final int lblGender=0x7f050009;
+        public static final int lblGoToLoginBtn=0x7f050014;
         public static final int lblLName=0x7f050008;
         public static final int lblPassword=0x7f050003;
+        public static final int lblRegConfimation=0x7f050013;
         public static final int lblRegister=0x7f05000e;
         public static final int lblUserName=0x7f050002;
         public static final int title_activity_register=0x7f050006;
+        public static final int title_activity_registration_confimation=0x7f050012;
     }
     public static final class style {
         /** 
