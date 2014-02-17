e15d61c03b01c888a22e562253ef36557723189d
vikrant23@gmail.com <vikrant23@gmail.com@607d1d21-f9f3-96d9-ef0f-9892642b3816>
2014-02-17 18:33:15 +0000

    Updated

--- trunk/MindYourTravel/src/com/example/mindyourtravel/LaunchActivity.java
+++ trunk/MindYourTravel/src/com/example/mindyourtravel/LaunchActivity.java
@@ -1,5 +1,11 @@
 package com.example.mindyourtravel;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -7,6 +13,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LaunchActivity extends Activity {
 
@@ -16,8 +23,10 @@ public class LaunchActivity extends Activity {
 		setContentView(R.layout.activity_launch);
 		final Button btnSingUp =(Button)findViewById(R.id.btnSingUp);
 		btnSingUp.setOnClickListener(onClickBtnSingUp);
-		final Button btnLogin =(Button)findViewById(R.id.btnLoginBack);
-		btnLogin.setOnClickListener(onClickBtnLogin);
+		final Button btnLoginSubmit =(Button)findViewById(R.id.btnLoginSubmit);
+		btnLoginSubmit.setOnClickListener(onClickBtnLogin);
+		
+		SetErrorLabelVisibility(View.INVISIBLE,R.string.lblError);
 	}
 
 	@Override
@@ -43,10 +52,64 @@ public class LaunchActivity extends Activity {
 		@Override
 		public void onClick(View view)
 		{
-			Intent intent = new Intent(view.getContext(),RegisterActivity.class);
+			final  TextView txtUserName = (TextView)findViewById(R.id.txtLogin);
+			final  TextView txtPassword = (TextView)findViewById(R.id.txtPassword);
 			
-			startActivity(intent);
+			try
+			{
+				JSONObject reqParameters= new JSONObject();
+				reqParameters.put("ULOGIN", txtUserName.getText());
+				reqParameters.put("UPASSWORD", txtPassword.getText());
+				
+				JsonHandler jsonHandler =JsonHandler.getInstance();
+				String url=jsonHandler.getFullUrl("UserLogin.php");
+				JSONObject result = jsonHandler.PostJsonDataToServer(url, reqParameters);
+				String resultCode= result.getString("RESULT");
+				
+				if(resultCode.contentEquals(AppConstant.PHPResponse_KO))
+				{
+					String errorCode=result.getString("ERRORCODE");
+					if(errorCode.contentEquals(AppConstant.PHP_ERROR_CODE.NOTEXISTS))
+					{
+						SetErrorLabelVisibility(View.VISIBLE,R.string.lblErrorUserNotExist);
+					}
+					else if(errorCode.contentEquals(AppConstant.PHP_ERROR_CODE.TECHNICAL))
+					{
+						SetErrorLabelVisibility(View.VISIBLE,R.string.lblErrorTechnical);
+					}
+				}
+				else
+				{
+					
+				}
+			}
+			catch(JSONException ex)
+			{
+				SetErrorLabelVisibility(View.VISIBLE,R.string.lblErrorTechnical);
+			}
+			catch (ClientProtocolException e)
+			{    
+				SetErrorLabelVisibility(View.VISIBLE,R.string.lblErrorTechnical);
+			}    
+			catch (IOException e) 
+			{    
+				SetErrorLabelVisibility(View.VISIBLE,R.string.lblErrorTechnical);
+			} 
+			catch(Exception e)
+			{
+				SetErrorLabelVisibility(View.VISIBLE,R.string.lblErrorTechnical);
+			}
 		}
 	};
+	
+	private void SetErrorLabelVisibility(int visibility,int errorResId)
+	{
+		TextView lblError =(TextView)findViewById(R.id.lblLoginErrorMsg);
+		if(lblError != null)
+		{
+			lblError.setVisibility(visibility);
+			lblError.setText(errorResId);
+		}
+	}
 
 }
