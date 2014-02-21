ffb755d8b07319f74650f305320cc34e5a0cee49
vikrant23@gmail.com <vikrant23@gmail.com@607d1d21-f9f3-96d9-ef0f-9892642b3816>
2014-02-21 18:15:54 +0000

    Updated

---
+++ trunk/MindYourTravel/src/com/example/mindyourtravel/AppConstant.java
@@ -0,0 +1,20 @@
+package com.example.mindyourtravel;
+
+public final class AppConstant {
+	public static final String PHPResponse_OK="OK";
+	public static final String PHPResponse_KO="KO";
+	public static final String DatabaseFile = "Users.db";
+	public static final String StorageFolder ="MindYourTravel";
+	
+	public static final class PHP_ERROR_CODE
+	{
+		//Error code to define record already exist
+		public static final String NOTEXISTS ="101";
+		//Error code to define record already exist
+		public static final String ALREADYEXISTS ="102";
+		//Error code to define technical error
+		public static final String TECHNICAL ="103";
+	}
+	
+
+}
