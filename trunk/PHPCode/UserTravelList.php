525b4fcd4ee8fb0c3a99eb35234037abf3e8569a
vikrant23@gmail.com <vikrant23@gmail.com@607d1d21-f9f3-96d9-ef0f-9892642b3816>
2014-03-09 15:48:59 +0000

    Updated

---
+++ trunk/PHPCode/UserTravelList.php
@@ -0,0 +1,37 @@
+<?php
+include "include/config.php";
+include "include/functions_db.php";
+
+$result=array();
+try
+{
+	
+	$jsonInput = json_decode(filter_var(file_get_contents('php://input'), FILTER_UNSAFE_RAW)); 
+	$CONUSERID = $jsonInput->CONUSERID;
+	
+	$queryResult = $db->Query("CALL aasv_user_travelmatch('".$CONUSERID."')");
+	if(mysql_num_rows($queryResult) > 0)
+	{
+		$result['RESULT'] = 'OK';
+		$resultSet= array();
+		while($row = mysql_fetch_array($queryResult)){
+			$resultSet[]=$row;
+		}
+		$result['DATAARRAY'] = $resultSet;
+	}
+	else
+	{
+		$result['RESULT'] = 'KO';
+		//Error code to define record is not exist
+		$result['ERRORCODE']="101";
+	}
+}
+catch(Exception $e)
+{
+   $result['RESULT'] = 'KO';
+   //Error code to define technical error
+   $result['ERRORCODE']="103";
+   $result['ERRMSG']=$e->getMessage();
+}
+echo json_encode($result);
+?>
---
+++ trunk/PHPCode/UserTravelPlan.php
@@ -0,0 +1,37 @@
+<?php
+include "include/config.php";
+include "include/functions_db.php";
+
+$result=array();
+try
+{
+	
+	$jsonInput = json_decode(filter_var(file_get_contents('php://input'), FILTER_UNSAFE_RAW)); 
+	$USERID = $jsonInput->USERID;
+	$CURRLOCATION = $jsonInput->CURRLOCATION;
+	$STARTLOCATION = $jsonInput->STARTLOCATION;
+	$ENDLOCATION = $jsonInput->ENDLOCATION;
+	$TRAVELTIME = $jsonInput->TRAVELTIME;
+	$TRAVELMODE = $jsonInput->TRAVELMODE;
+	$NOOFPASSENGER = $jsonInput->NOOFPASSENGER;
+	$queryResult = $db->Query("CALL aasv_travelplan_insert('".$USERID."','".$CURRLOCATION."','".$STARTLOCATION."','".$ENDLOCATION."','".$TRAVELTIME."','".$TRAVELMODE."','".$NOOFPASSENGER."')");
+	if(mysql_num_rows($queryResult) > 0)
+	{
+		$result['RESULT'] = 'OK';
+	}
+	else
+	{
+		$result['RESULT'] = 'KO';
+		//Error code to define record is not exist
+		$result['ERRORCODE']="101";
+	}
+}
+catch(Exception $e)
+{
+   $result['RESULT'] = 'KO';
+   //Error code to define technical error
+   $result['ERRORCODE']="103";
+   $result['ERRMSG']=$e->getMessage();
+}
+echo json_encode($result);
+?>
