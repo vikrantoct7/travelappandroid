af0b1498d28a3d1274eb7b701aa0be143ad7f9a7
vikrant23@gmail.com <vikrant23@gmail.com@607d1d21-f9f3-96d9-ef0f-9892642b3816>
2014-07-05 06:43:48 +0000

    updated

---
+++ trunk/LetsGO/src/com/example/LetsGO/TextViewValidator.java
@@ -0,0 +1,39 @@
+package com.example.LetsGO;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.widget.TextView;
+
+
+public class TextViewValidator extends EditTextValidator implements OnFocusChangeListener{
+
+	public TextViewValidator(TextView textView) {
+		super(textView);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean validate(TextView textView)
+	{
+		boolean result;
+		if(textView.getText().length()==0)
+		{
+			textView.setError("This is mandatory!");
+			result=false;
+		}
+		else
+		{
+			textView.setError(null);
+			result=true;
+		}
+		return result;
+	}
+		
+	@Override
+	public void onFocusChange(View textView, boolean hasFocus) {
+		// TODO Auto-generated method stub
+		if(!hasFocus)
+		{
+			validate((TextView)textView);
+		}
+	}
+
+}
---
+++ trunk/LetsGO/src/com/example/LetsGO/UserDTO.java
@@ -0,0 +1,115 @@
+package com.example.LetsGO;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String contactNo;
+	private int gender;
+	private int age;
+	private int travelId;
+	private int cityId; 
+	private boolean isAppLoginUser;
+	
+	public String getFirstName()
+	{
+		return this.firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+	
+	public String getLastName()
+	{
+		return this.lastName;
+	}
+	
+	public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+	
+	public int getCityId()
+	{
+		return this.cityId;
+	}
+	
+	public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+	
+	public String getContactNo()
+	{
+		return this.contactNo;
+	}
+	
+	public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getTravelId() {
+		return travelId;
+	}
+
+	public void setTravelId(int travelId) {
+		this.travelId = travelId;
+	}
+
+	public boolean isAppLoginUser() {
+		return isAppLoginUser;
+	}
+
+	public void setAppLoginUser(boolean isAppLoginUser) {
+		this.isAppLoginUser = isAppLoginUser;
+	}
+	
+	public String getUserFullName()
+	{
+		return this.firstName + " "+  this.lastName;
+	}
+	
+	public String getGenderStringValue()
+	{
+		String genderStr="M";
+		if(this.gender ==1)
+		{
+			genderStr="M";
+		}
+		else if(this.gender ==2)
+		{
+			genderStr="F";
+		}
+		return genderStr;
+	}
+}
