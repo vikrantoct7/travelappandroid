31d5e443d5f3435d0ebc22e49dc8a3f27f35e5a7
vikrant23 <vikrant23@gmail.com>
2014-08-30 18:17:44 +0000

    rename letsGO to LetsGoo

---
+++ LetsGoo/src/com/example/LetsGoo/GenericValidator.java
@@ -0,0 +1,23 @@
+package com.example.LetsGoo;
+
+import android.widget.TextView;
+
+public  class GenericValidator {
+	
+	public boolean validate(TextView textView)
+	{
+		boolean result;
+		if(textView.getText().length()==0)
+		{
+			textView.setError("This is mandatory!");
+			result=false;
+		}
+		else
+		{
+			textView.setError(null);
+			result=true;
+		}
+		return result;
+	}
+
+}
