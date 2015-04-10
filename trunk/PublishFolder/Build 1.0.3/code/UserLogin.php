68f472c591e4cc7f9f0ea1afe42abf251e2e5330
vikrant23@gmail.com <vikrant23@gmail.com@607d1d21-f9f3-96d9-ef0f-9892642b3816>
2015-04-10 17:24:24 +0000

    verson 1.0.3 uploaded

---
+++ trunk/PublishFolder/Build 1.0.3/code/UserLogin.php
@@ -0,0 +1,35 @@
+<?php
+include "include/config.php";
+include "include/functions_db.php";
+
+$result=array();
+try
+{
+	
+	$jsonInput = json_decode(filter_var(file_get_contents('php://input'), FILTER_UNSAFE_RAW)); 
+	$UMOBILENO = $jsonInput->UMOBILENO;
+	$queryResult = $db->Query("CALL aasv_user_getloginuserdata('".$UMOBILENO."')");
+	if(mysql_num_rows($queryResult) > 0)
+	{
+		$result['RESULT'] = 'OK';
+		while($row = mysql_fetch_array($queryResult)){
+			$result['USERDATA'] = $row;
+		}
+	}
+	else
+	{
+		$result['RESULT'] = 'KO';
+		//Error code to define record is not exist
+		$result['ERRORCODE']="101";
+	}
+	$db->FreeResult();
+}
+catch(Exception $e)
+{
+   $result['RESULT'] = 'KO';
+   //Error code to define technical error
+   $result['ERRORCODE']="103";
+   $result['ERRMSG']=$e->getMessage();
+}
+echo json_encode($result);
+?>
